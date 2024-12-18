94a26b57451a1306f7d266a7a9a05f8637acc518
HoangPham3003 <hoang.ph303@gmail.com>
2024-12-18 10:42:43 +0200

    update: change module name

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,8 @@
-import Logo from "@/components/logo";
+import Logo from "@/components/Logo";
 import { ProfileCard } from "@/components/cards/profiles";
 import { Container } from '@/components/ui/container'
 import { Intro } from '@/components/cards/profiles/intro';
-import { FooterBottom } from "@/components/footer";
+import { FooterBottom } from "@/components/Footer";
 
 export default function Home() {
     return (
