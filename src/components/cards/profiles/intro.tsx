8163d5c27dbd7ffae9ffb272cdd789d7b6c0cfaf
HoangPham3003 <hoang.ph303@gmail.com>
2024-12-22 08:05:20 +0200

    update: nav link home

--- src/components/cards/profiles/intro.tsx
+++ src/components/cards/profiles/intro.tsx
@@ -12,7 +12,7 @@ export function Intro() {
                 I am from <img className="mx-2" src="https://cdn-icons-png.flaticon.com/512/555/555515.png" width={20} height={18}></img> <b className="text-[#03FFF7]">Hanoi, Vietnam</b>, currently living in <img className="mx-2" src="https://cdn-icons-png.flaticon.com/512/555/555627.png" width={20} height={18}></img> <b className="text-[#03FFF7]">Joensuu, Finland</b>.
             </p>
             <p className="my-5">
-                Pursuing Master's degree in Computer Science at <a className="bg-black text-white p-1 text-sm hover:text-[#03FFF7] underline decoration-[#03FFF7]" href="https://www.uef.fi/en">University of Eastern Finland</a> , I am passionate about <b className="text-[#03FFF7]">Artificial Intelligence</b> and excited by its potential to the future.
+                Pursuing Master's degree in Computer Science at <a className="bg-black text-white p-1 text-sm hover:text-[#03FFF7] underline decoration-[#03FFF7]" href="https://www.uef.fi/en" target="_blank">University of Eastern Finland</a> , I am passionate about <b className="text-[#03FFF7]">Artificial Intelligence</b> and excited by its potential to the future.
             </p>
             <p className="my-5">
                 Know more about me at:
